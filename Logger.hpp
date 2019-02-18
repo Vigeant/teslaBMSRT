bb23f12d76b11fd9bae2ccaffd14dcaf93130cd8
Guil-T <Vigeant@users.noreply.github.com>
2019-02-17 21:12:08 -0500

    simplified logger

--- Logger.hpp
+++ Logger.hpp
@@ -1,31 +1,34 @@
-/*
- * Logger.h
- *
- Copyright (c) 2013 Collin Kidder, Michael Neuweiler, Charles Galpin
-
- Permission is hereby granted, free of charge, to any person obtaining
- a copy of this software and associated documentation files (the
- "Software"), to deal in the Software without restriction, including
- without limitation the rights to use, copy, modify, merge, publish,
- distribute, sublicense, and/or sell copies of the Software, and to
- permit persons to whom the Software is furnished to do so, subject to
- the following conditions:
-
- The above copyright notice and this permission notice shall be included
- in all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
- */
 #ifndef LOGGER_HPP_
 #define LOGGER_HPP_
-#include "Log.hpp"
+
+#include <Arduino.h>
+#include <ChRt.h>
+#include "CONFIG.h"
+
+
+class Logger {
+public:
+    enum LogLevel {
+        Debug = 0, Info = 1, Warn = 2, Error = 3, Off = 4, Cons = 5
+    };
+    Logger();
+    void debug(const char *, ...);
+    void info(const char *, ...);
+    void warn(const char *, ...);
+    void error(const char *, ...);
+    void console(const char *, ...);
+    void setLoglevel(LogLevel);
+    LogLevel getLogLevel();
+    uint32_t getLastLogTime();
+    boolean isDebug();
+private:
+    LogLevel logLevel;
+    uint32_t lastLogTime;
+    MUTEX_DECL(serial_MTX);
+    //mutex_t serial_MTX;
+    void log(LogLevel, const char *format, va_list);
+    void logMessage(const char *format, va_list args);
+};
 
 //export the logger
 extern Logger log_inst;
@@ -37,4 +40,4 @@ extern Logger log_inst;
 #define LOG_CONSOLE log_inst.console
 //#define LOG_CONSOLE SERIALCONSOLE.print
 
-#endif /* LOGGER_HPP_ */
+#endif /* LOG_HPP_ */
